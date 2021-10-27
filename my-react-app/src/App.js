c685f66c3ae415dc7ddd84c2b82d696a741ed646
lucy3505 <415492767@qq.com>
2021-10-27 20:38:54 +0800

    02render函数返回值

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-
+import returnV from "./assets/img/1.png";
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <img src={returnV} />
+      </div>
+    );
   }
 }
