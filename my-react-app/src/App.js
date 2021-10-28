66786d66af0d62db2edd830ee259ff24bf0e1d29
lucy3505 <415492767@qq.com>
2021-10-28 10:11:55 +0800

    add lifecircle img

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-
+import lifecircle from "./assets/img/2.png";
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <img src={lifecircle} />
+      </div>
+    );
   }
 }
