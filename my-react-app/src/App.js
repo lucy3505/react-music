7d83e4bbf57bc310777e31c215609bedb91a830a
lucy3505 <415492767@qq.com>
2021-10-28 10:22:36 +0800

    lifecircle02

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import lifecircle from "./assets/img/2.png";
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("执行了组件的constructor");
+  }
+
+  componentDidMount() {
+    console.log("执行了组件的componentDiDMount方法");
+  }
+
   render() {
+    console.log("执行了组件的render函数 ");
     return (
       <div>
         <img src={lifecircle} />
+        <p>我是app组件</p>
       </div>
     );
   }
 }
+
+export default App;
