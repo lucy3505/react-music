0c6e2050c338e4431b7b14f317f5361520ed5d89
lucy3505 <415492767@qq.com>
2021-12-17 22:33:08 +0800

    done

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-
+import App from "./../src/4组件通信案例/App";
+// import "../src/4组件通信案例/style.css";
 ReactDOM.render(
   <React.StrictMode>
     <App />
