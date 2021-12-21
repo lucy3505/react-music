25e92b04d89862ab2e8973a3631b8745b031a28d
lucy3505 <415492767@qq.com>
2021-12-21 11:07:44 +0800

    done

--- my-react-app/src/09_前面知识点补充/02_全局事件传递.js
+++ my-react-app/src/09_前面知识点补充/02_全局事件传递.js
@@ -1,7 +1,48 @@
 import React, { PureComponent } from "react";
 
+import { EventEmitter } from "events";
+//事件总线:eventBus
+
+const eventBus = new EventEmitter();
+class Home extends PureComponent {
+  componentDidMount() {
+    eventBus.addListener("sayHello", this.handleSayHelloListener);
+  }
+
+  componentWillUnmount() {
+    eventBus.removeListener("sayHello", this.handleSayHelloListener);
+  }
+
+  handleSayHelloListener(message, num) {
+    console.log(message, num);
+  }
+
+  render() {
+    return <div>Home</div>;
+  }
+}
+class Profile extends PureComponent {
+  render() {
+    return (
+      <div>
+        Profile
+        <button onClick={(e) => this.emitEvent()}>点击了profile按钮</button>
+      </div>
+    );
+  }
+
+  emitEvent() {
+    console.log(11);
+    eventBus.emit("sayHello", "hello home", 123);
+  }
+}
 export default class App extends PureComponent {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Home />
+        <Profile />
+      </div>
+    );
   }
 }
