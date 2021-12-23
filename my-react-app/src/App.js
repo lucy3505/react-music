3f2039b229299a4954d74abc769e661602069dc6
lucy3505 <415492767@qq.com>
2021-12-23 20:58:21 +0800

    process

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,17 +1,7 @@
 import React, { PureComponent } from "react";
-import CSSTransitionDemo from "./transition/CSSTransitionDemo";
-import SwitchTransitionDemo from "./transition/SwitchTransitionDemo";
-import TransitionGroupDemo from "./transition/TransitionGroupDemo";
-//SwitchTransitionDemo 两个组件之间切换的炫酷动画
-//CSSTransitionDemo一个组件进入和离开时的动画
+
 export default class App extends PureComponent {
   render() {
-    return (
-      <div style={{ textAlign: "center", padding: "30px" }}>
-        {/* <CSSTransitionDemo /> */}
-        <TransitionGroupDemo />
-        {/* <SwitchTransitionDemo /> */}
-      </div>
-    );
+    return <div style={{ textAlign: "center", padding: "30px" }}>a</div>;
   }
 }
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import "antd/dist/antd.less";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
