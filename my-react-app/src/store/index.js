3b04cd9dd6d267abe58602323c4ba7f151fd0cbc
lucy3505 <415492767@qq.com>
2022-02-11 10:04:09 +0800

    change redux

--- my-react-app/src/store/index.js
+++ my-react-app/src/store/index.js
@@ -3,7 +3,9 @@ import thunk from "redux-thunk";
 import reducer from "./reducer";
 
 const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) || compose;
+  (window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true })) ||
+  compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
